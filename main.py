034bf90dc9656de6d8d6d082f07ed3472c269a82
Unknown <afranciscogg@gmail.com>
2019-05-17 20:48:45 +0100

    Filename to insert results

--- main.py
+++ main.py
@@ -12,10 +12,12 @@ if __name__ == '__main__':
 	k_elite = 0.1
 	sel_parents = tour_sel(k_tour)
 	recombination = order_cross
+	#Change variables mutation here
 	mutation = muta_swap
+
 	sel_survivors = sel_survivors_elite(k_elite)
 
-	numb_runs = 1
+	numb_runs = 30
 
 	coord = le_coordenadas_tsp('uy734.tsp')
 	dicio = dicio_cidades(coord)
@@ -23,10 +25,10 @@ if __name__ == '__main__':
 	fitness_func = meu_merito
 	size_cromo = len(dicio)
 
+
+	filename = str(numb_generations)+'_'+str(size_pop)+'_'+str(prob_mut)+'_'+str(prob_cross)+'_'+str(k_tour)+'_'+str(k_elite)+'swapmutation.txt'
+	filename = 'data/'+filename
+
 	start = time.time()
-	boa,average_best = run(numb_runs,numb_generations-1,size_pop,size_cromo,prob_mut,prob_cross,sel_parents,recombination,mutation,sel_survivors,fitness_func)
+	run_for_file(filename,numb_runs,numb_generations-1,size_pop,size_cromo,prob_mut,prob_cross,sel_parents,recombination,mutation,sel_survivors,fitness_func)
 	end = time.time()
-
-
-	print (end - start)
-	display_stat_n(boa,average_best)
