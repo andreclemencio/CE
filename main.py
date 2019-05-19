952a4e3861606721ac03624d4762d5ec15031488
Unknown <afranciscogg@gmail.com>
2019-05-19 17:49:32 +0100

    Statistic tests

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tsp import *
 from n_queens import *
 import time
 from utils import *
+import scipy.stats as stats
 #from stats import *
 
 if __name__ == '__main__':
@@ -34,7 +35,88 @@ if __name__ == '__main__':
 	size_cromo = size_problem
 
 
-	filename = str(numb_generations)+'_'+str(size_pop)+'_'+str(prob_mut)+'_'+str(prob_cross)+'_'+str(k_tour)+'_'+str(k_elite)+'swapmutation_nqueens.txt'
-	filename = 'data/'+filename
+	#filename = str(numb_generations)+'_'+str(size_pop)+'_'+str(prob_mut)+'_'+str(prob_cross)+'_'+str(k_tour)+'_'+str(k_elite)+'swapmutation_nqueens.txt'
+	#filename = 'data/'+filename
 
-	run_for_file(filename,numb_runs,numb_generations-1,size_pop,size_cromo,prob_mut,prob_cross,sel_parents,recombination,mutation,sel_survivors,fitness_func)
+	#run_for_file(filename,numb_runs,numb_generations-1,size_pop,size_cromo,prob_mut,prob_cross,sel_parents,recombination,mutation,sel_survivors,fitness_func)
+	
+	swap_tsp = 'data/250_50_0.1_0.8_3_0.1swapmutation.txt'
+	swap_nqueens = 'data/250_50_0.1_0.8_3_0.1swapmutation_nqueens.txt'
+	inversion_tsp = 'data/250_50_0.1_0.8_3_0.1inversionmutation.txt'
+	inversion_nqueens = 'data/250_50_0.1_0.8_3_0.1inversionmutation_nqueens.txt'
+	scramble_tsp = 'data/250_50_0.1_0.8_3_0.1scramblemutation.txt'
+	scramble_nqueens = 'data/250_50_0.1_0.8_3_0.1scramblemutation_nqueens.txt'
+	
+	
+	swap_tsp_data = read_file(swap_tsp)
+	swap_nqueens_data = read_file(swap_nqueens)
+	inversion_tsp_data = read_file(inversion_tsp)
+	inversion_nqueens_data = read_file(inversion_nqueens)
+	scramble_tsp_data = read_file(scramble_tsp)
+	scramble_nqueens_data = read_file(scramble_nqueens)
+	
+	#BOXPLOTS
+	box_plt_gr_tsp(swap_tsp_data, inversion_tsp_data, scramble_tsp_data)
+	box_plt_gr_nqueens(swap_nqueens_data, inversion_nqueens_data, scramble_nqueens_data)
+	
+	#HISTOGRAMS
+	#histogram(swap_tsp_data, 'Swap Mutation TSP', 'Fitness','')
+	#histogram(inversion_tsp_data, 'Inversion Mutation TSP', 'Fitness','')
+	#histogram(scramble_tsp_data, 'Scramble Mutation TSP', 'Fitness','')
+	#histogram(swap_nqueens_data, 'Swap Mutation Nqueens', 'Fitness','')
+	#histogram(inversion_nqueens_data, 'Inversion Mutation Nqueens', 'Fitness','')
+	#histogram(scramble_nqueens_data, 'Scramble Mutation Nqueens', 'Fitness','')
+	
+	#Testar se podemos realizar testes paramétricos
+	#Kolgomorov-Smirnov
+	print("Teste Paramétrico [KS] para [SWAP-TSP]: ")
+	data_test = ks_test(swap_tsp_data)
+	print(data_test)
+	print("Teste Paramétrico [KS] para [INVERSION-TSP]: ")
+	data_test = ks_test(inversion_tsp_data)
+	print(data_test)
+	print("Teste Paramétrico [KS] para [SCRAMBLE-TSP]: ")
+	data_test = ks_test(scramble_tsp_data)
+	print(data_test)
+	
+	print("Teste Paramétrico [KS] para [SWAP-NQUEENS]: ")
+	data_test = ks_test(swap_nqueens_data)
+	print(data_test)
+	print("Teste Paramétrico [KS] para [INVERSION-NQUEENS]: ")
+	data_test = ks_test(inversion_nqueens_data)
+	print(data_test)
+	print("Teste Paramétrico [KS] para [SCRAMBLE-NQUEENS]: ")
+	data_test = ks_test(scramble_nqueens_data)
+	print(data_test)
+	
+	#Shapiro-Wilk
+	print("Teste Paramétrico [SH] para [SWAP-TSP]: ")
+	data_test = sw_test(swap_tsp_data)
+	print(data_test)
+	print("Teste Paramétrico [SH] para [INVERSION-TSP]: ")
+	data_test = sw_test(inversion_tsp_data)
+	print(data_test)
+	print("Teste Paramétrico [SH] para [SCRAMBLE-TSP]: ")
+	data_test = sw_test(scramble_tsp_data)
+	print(data_test)
+	
+	print("Teste Paramétrico [SH] para [SWAP-NQUEENS]: ")
+	data_test = sw_test(swap_nqueens_data)
+	print(data_test)
+	print("Teste Paramétrico [SH] para [INVERSION-NQUEENS]: ")
+	data_test = sw_test(inversion_nqueens_data)
+	print(data_test)
+	print("Teste Paramétrico [SH] para [SCRAMBLE-NQUEENS]: ")
+	data_test = sw_test(scramble_nqueens_data)
+	print(data_test)
+	
+	#Levene Teste
+	levene_output = levene(swap_tsp_data, inversion_tsp_data, scramble_tsp_data)
+	print(levene_output)
+	levene_output = levene(swap_nqueens_data, inversion_nqueens_data, scramble_nqueens_data)
+	print(levene_output)
+	
+	#Kruskal-Wallis
+	print(stats.kruskal(swap_tsp_data, inversion_tsp_data, scramble_tsp_data))
+	
+	
--- utils.py
+++ utils.py
@@ -4,6 +4,8 @@ Ernesto Costa, February 2016
 """
 
 import matplotlib.pyplot as plt
+import numpy as np
+import scipy.stats as stats
 
 # auxiliary 
 def display(indiv, phenotype):
@@ -28,9 +30,72 @@ def display_stat_n(boa,average_best):
     plt.plot(generations,average_best,label='Average of Bests')
     plt.legend(loc='best')
     plt.show()
-
     
+#Method to Read Files
+def read_file(filename):
+    f = open(filename, 'r')
+    x = f.readlines()
+    f.close()
+    data = []
+    for i in range(0,len(x)-1):
+        data.append(float(x[i]))
+    return data
 
+
+#Make Boxplots
+def box_plt_gr_tsp(swap_data, inversion_data, scramble_data):
+    data_analyse = [swap_data, inversion_data, scramble_data]
+    fig = plt.figure(1)
+    ax = fig.add_subplot(111)
+    ax.boxplot(data_analyse)
+    ax.set_xticklabels(['SWAP', 'INV','SCRA'])
+    plt.ylabel('TSP Benchmark')
+    ax.set_yticks(np.arange(200000, 290000, 10000))
+    ax.get_xaxis().tick_bottom()
+    ax.get_yaxis().tick_left()
+    plt.show()   
+    
+def box_plt_gr_nqueens(swap_data, inversion_data, scramble_data):
+    data_analyse = [swap_data, inversion_data, scramble_data]
+    fig = plt.figure(2)
+    ax = fig.add_subplot(111)
+    ax.boxplot(data_analyse)
+    ax.set_xticklabels(['SWAP', 'INV','SCRA'])
+    plt.ylabel('NQUEENS Benchmark')
+    ax.set_yticks(np.arange(30, 90, 10))
+    ax.get_xaxis().tick_bottom()
+    ax.get_yaxis().tick_left()
+    plt.show() 
+
+#Just to check data (if follow normal distribution)
+def histogram(data_analyse, title, x, y, bins = 30):
+    fig = plt.figure(3)
+    #fit =  stats.norm.pdf(data_analyse, np.mean(data_analyse), np.std(data_analyse))
+    #plt.plot(data_analyse,fit,'-o')
+    plt.hist(data_analyse, bins=bins)
+    plt.title(title)
+    plt.xlabel(x)
+    plt.ylabel(y)
+    plt.show()
+    
+#Kolgomorov-Smirnov
+def ks_test(data_analyse):
+    norm_data = (data_analyse - np.mean(data_analyse))/(np.std(data_analyse)/np.sqrt(len(data_analyse)))
+    return stats.kstest(norm_data, 'norm')    
+
+#Shapiro-Wilk
+def sw_test(data_analyse):
+    norm_data = (data_analyse - np.mean(data_analyse))/(np.std(data_analyse)/np.sqrt(len(data_analyse)))
+    return stats.shapiro(norm_data)
+
+#Levene
+def levene(swap, inversion, scramble):
+    output = stats.levene(swap, inversion, scramble, center='mean')
+    return output
+    
+#Kruskal-Wallis
+def kruskalwallis(swap, inversion, scramble):
+    output = stats.kruskal(swap,inversion,scramble)
     
 if __name__ == '__main__':
     pass
